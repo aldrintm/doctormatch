65abfe2af3d04df9da64e75ae5d5c9a008208c6b
Aldrin <aldrin@plamarusa.com>
2025-04-06 13:47:44 -0700

    Random Changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,21 @@ export default function Home() {
     <div className='flex h-screen max-h-screen'>
       <section className='remove-scrollbar container my-auto'>
         <div className='sub-container max-w-[496px]'>
-          <Image
-            src='/assets/docmatchlogo-full.png'
-            height={1000}
-            width={1000}
-            alt='patient'
-            className='mb-12 h-20 w-fit'
-          ></Image>
+          <div className='flex justify-center'>
+            <div className='bg-white w-2/3 mb-12 pl-2 flex items-center justify-center rounded-full'>
+              <Image
+                src='/assets/docmatchlogo-full.png'
+                height={1000}
+                width={1000}
+                alt='patient'
+                className='h-20 w-fit'
+              />
+            </div>
+          </div>
           <PatientForm />
           <div className='text-14-regular mt-20 flex justify-between'>
             <p className='justify-items-end text-dark-600 xl:text-left'>
-              © 2025 Doctor Match Copyright
+              © 2025 Copyright - Doctor Match
             </p>
             <Link href='/?admin=true' className='text-green-500'>
               Admin
@@ -28,7 +32,7 @@ export default function Home() {
       </section>
 
       <Image
-        src='/assets/images/onboarding-img.png'
+        src='/assets/images/onboarding-img2.webp'
         height={1000}
         width={1000}
         alt='patient'
